c4ea4f7b7b84ab45a25e872eecf047e902b20010
Imanol Perez <iperezadam1B@gmail.com>
2024-01-16 16:46:43 +0100

    Proyecto creado

--- Layuots 1 de Imanol/app/src/main/java/com/example/layuots1deimanol/FrameLayoutActivity.kt
+++ Layuots 1 de Imanol/app/src/main/java/com/example/layuots1deimanol/FrameLayoutActivity.kt
@@ -3,7 +3,7 @@ package com.example.layuots1deimanol
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FrameLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.frame_layout)
---
+++ Layuots 1 de Imanol/app/src/main/java/com/example/layuots1deimanol/LinearLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.example.layuots1deimanol
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+
+}
