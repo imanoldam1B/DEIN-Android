818f863bf3b97b728758342f4260af8e54c6a9b7
Imanol Perez <iperezadam1B@gmail.com>
2024-01-16 19:43:39 +0100

    Primera ventana finalizada perfecta

--- Layouts2deImanol/app/build.gradle.kts
+++ Layouts2deImanol/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.layouts2deimanol"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.layouts2deimanol"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
