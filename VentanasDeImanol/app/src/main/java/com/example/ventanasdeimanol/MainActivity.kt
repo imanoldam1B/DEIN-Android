1864ddf76032b710799a12c460386bc4d7ee6897
Imanol Perez <iperezadam1B@gmail.com>
2024-01-29 16:16:25 +0100

    En proceso de la segunda ventana

--- VentanasDeImanol/app/src/main/java/com/example/ventanasdeimanol/MainActivity.kt
+++ VentanasDeImanol/app/src/main/java/com/example/ventanasdeimanol/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.ventanasdeimanol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.content.Intent
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var txtNombre: EditText
+    private lateinit var btnAceptar: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtNombre = findViewById(R.id.txtNombre)
+        btnAceptar = findViewById(R.id.btnAceptar)
+
+        //Implementamos el evento click del botón
+        btnAceptar.setOnClickListener {
+
+            //Creamos el intent
+            val intent = Intent(this@MainActivity, SaludoActivity::class.java)
+
+            //Creamos la información a pasar entre actividades
+            val b = Bundle()
+            b.putString("NOMBRE", txtNombre.text.toString())
+
+            //Añadimos la información al intent
+            intent.putExtras(b)
+
+            //Iniciamos la nueva actividad
+            startActivity(intent)
+        }
     }
 }
