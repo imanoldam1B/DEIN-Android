dd2e8ef23502342b42208c783f71bd2c63fd267e
Imanol Perez <iperezadam1B@gmail.com>
2024-01-29 20:51:40 +0100

    Cambios en segunda ventana, falta todavía

--- VentanasDeImanol/app/src/main/java/com/example/ventanasdeimanol/SaludoActivity.kt
+++ VentanasDeImanol/app/src/main/java/com/example/ventanasdeimanol/SaludoActivity.kt
@@ -2,10 +2,22 @@ package com.example.ventanasdeimanol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SaludoActivity : AppCompatActivity() {
+
+    private lateinit var txtSaludo: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saludo)
+
+        //Localizar los controles
+        txtSaludo = findViewById(R.id.txtSaludo)
+
+        //Recuperamos la información pasada en el intent
+        val bundle = intent.extras
+
+        //Construimos el mensaje a mostrar
+        txtSaludo.text = "Hola ${bundle?.getString("NOMBRE")}"
     }
 }
