65c52f172c82bd986861f21b768002c0a35c582d
Imanol Perez <iperezadam1B@gmail.com>
2024-01-29 15:46:59 +0100

    Codigo primera ventana finalizado, segunda ventana creada

---
+++ VentanasDeImanol/app/src/main/java/com/example/ventanasdeimanol/SaludoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ventanasdeimanol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaludoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+    }
+}
