e51386c6ce8ecb5dae20a13c8af12bb7794bbf24
Imanol Perez <iperezadam1B@gmail.com>
2024-02-01 19:33:18 +0100

    Proyecto creado

---
+++ Imanol_Menu_Popup/app/src/main/java/com/example/imanol_menu_popup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.imanol_menu_popup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
